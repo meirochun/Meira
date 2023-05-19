d4cb2cb328de1bb7aededf6662394e3a98bec604
João Marcos Corrêa <desenvolvimento9@softin.com.br>
2023-05-19 19:35:57 -0300

    Removing valorant api, maybe for now.

--- Config/ConfigJSON.cs
+++ Config/ConfigJSON.cs
@@ -8,7 +8,5 @@ namespace Meira.Config
         public string Token { get; private set; }
         [JsonProperty("prefix")]
         public string Prefix { get; private set; }
-        [JsonProperty("valorantApiCall")]
-        public string ValorantApiCall { get; private set; }
     }
 }
