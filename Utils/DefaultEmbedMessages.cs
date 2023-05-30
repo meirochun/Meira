38ea7b784a81704a4211dc2100aaecaf21355dfa
meirochun <joaocorrea0758@gmail.com>
2023-05-30 16:18:54 -0300

    feat: new default embed message added

--- Utils/DefaultEmbedMessages.cs
+++ Utils/DefaultEmbedMessages.cs
@@ -6,6 +6,8 @@ namespace Meira.Utils
 {
     internal class DefaultEmbedMessages : IDefaultEmbedMessages
     {
+        public DiscordEmbedBuilder EmbedMessage { get; private set; } = null;
+
         /// <summary>
         /// Returns an embed message warning the user that the command is only available for administrators
         /// </summary>
@@ -13,14 +15,27 @@ namespace Meira.Utils
         /// <returns></returns>
         public async Task NonAdminMessage(InteractionContext ctx)
         {
-            var nonAdminMessage = new DiscordEmbedBuilder()
-            {
-                Title = "Access Denied",
-                Description = "You must be an administrator to use this command",
-                Color = DiscordColor.Red
-            };
+            EmbedMessage
+                .WithTitle("Access Denied")
+                .WithDescription("You must be an administrator to use this command")
+                .WithColor(DiscordColor.Red);
 
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(nonAdminMessage));
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(EmbedMessage));
+        }
+
+        /// <summary>
+        /// Returns an embed message warning the user doens't have enough permissions to use the command
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <returns></returns>
+        public async Task NoPermissionEnough(InteractionContext ctx)
+        {
+            EmbedMessage
+                .WithTitle("Access Denied")
+                .WithDescription("You don't have enough permissions to use this command")
+                .WithColor(DiscordColor.Red);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(EmbedMessage));
         }
     }
 }
