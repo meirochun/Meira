ed741a654dc0f4257e9623dc6e2e7d3331263406
meirochun <meirochun@gmail.com>
2024-01-18 17:54:28 -0300

    Making everything more interesting.

--- meirabot.py
+++ meirabot.py
@@ -1,26 +1,71 @@
-import discord
-from discord import app_commands
-from dotenv import load_dotenv
 import os
+import sys
+import json
+import platform
 
-class client(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False
+import meira.tools.setup_log as meira_log
 
-    async def on_ready(self):
-        if not self.synced:
-            await tree.sync()
-            self.synced = True
-        print('Meira rodando!')
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
-aclient = client()
-tree = app_commands.CommandTree(aclient)
+if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+    sys.exit("'config.json' não encontrado! Por favor, adicione-o e tente novamente.")
+else:
+    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+        config = json.load(file)
 
-@tree.command(name = 'ping', description = 'Verifica se o bot está funcionando.')
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message('Pong!', ephemeral = True)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 
+class MeiraBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(config["prefix"]),
+            intents=intents,
+            help_command=None,
+        )
+        self.logger = meira_log.logger
+        self.config = config
+
+    async def load_cogs(self) -> None:
+        for file in os.listdir("./cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Carregando extensão '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(f"Não foi possível carregar a extensão {extension} devido a {exception}")
+
+    async def setup_hook(self) -> None:
+        """
+        Setup hook para carregar as extensões.
+        """
+        await self.load_cogs()
+
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        Evento chamado quando o bot recebe uma mensagem.
+        Impede que o bot responda a si mesmo e a outros bots.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+        await self.process_commands(message)
+
+    async def on_ready(self) -> None:
+        self.logger.info(f"Logado como {self.user.name}")
+        self.logger.info(f"Sistema operacional: {platform.system()} {platform.release()}")
+        self.logger.info(f"Python: {platform.python_version()}")
+        self.logger.info(f"Discord.py: {discord.__version__}")
+        self.logger.info("-----------------------------")
+        synced = await self.tree.sync()
+        self.logger.info(f"Slash commands sincronizados: {str(len(synced))}.")
+        self.logger.info("-----------------------------")
+        await bot.change_presence(activity=discord.Game(name="Meirando..."), status=discord.Status.idle)
 
 load_dotenv()
-aclient.run(os.getenv('TOKEN'))
+bot = MeiraBot()
+bot.run(os.getenv('TOKEN'))
