8b66f2f356d7feef28be4793ccf8eb7bd3ad3c13
meirochun <meirochun@gmail.com>
2024-01-21 03:02:11 -0300

    More thread commands included.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,4 @@
-from time import sleep
+from meira.default_responses import thread_default_responses as thread_responses
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -8,17 +8,19 @@ class Moderation(commands.Cog, name="moderation"):
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
 
-    @commands.command()
+    @commands.command(name="clear", description="Apaga mensagens (permissão necessária)")
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
     async def clear(self, ctx: Context, quantidade: int) -> None:
         clear_messages = await ctx.channel.purge(limit=quantidade, bulk = quantidade > 5)
         
         embed = discord.Embed(
-            description=f"**{len(clear_messages)}** mensagens foram apagadas. \💀",
+            description=f"**{len(clear_messages)}** mensagens foram apagadas. \✅",
             color=discord.Color.green()
         )
         await ctx.send(embed=embed)
 
-    @app_commands.command()
+    @app_commands.command(name="create_thread", description="Cria uma thread")
     @app_commands.describe(
         nome = "Nome da thread",
         mensagem_id = "ID da mensagem que será enviada para a thread"
@@ -36,6 +38,50 @@ class Moderation(commands.Cog, name="moderation"):
                 name=nome,
                 type=discord.ChannelType.public_thread)
             await interaction.followup.send(f"Thread criada: {created_thread.mention}", ephemeral=True)
+
+    @app_commands.command(name="lock_thread", description="Trava uma thread")
+    async def lock_thread(self, interaction: discord.Interaction) -> None:
+        if interaction.channel.type != discord.ChannelType.public_thread:
+            await thread_responses.only_executable_in_threads(interaction, True)
+            return
+        await interaction.channel.edit(locked=True)
+        await thread_responses.locked_thread(interaction, False)
+    
+    @app_commands.command(name="unlock_thread", description="Destrava uma thread")
+    async def unlock_thread(self, interaction: discord.Interaction) -> None:
+        if interaction.channel.type != discord.ChannelType.public_thread:
+            await thread_responses.only_executable_in_threads(interaction, True)
+            return
+        await interaction.channel.edit(locked=False)
+        await thread_responses.unlocked_thread(interaction, False)
+
+    @app_commands.command(name="archive_thread", description="Arquiva uma thread")
+    async def archive_thread(self, interaction: discord.Interaction) -> None:
+        if interaction.channel.type != discord.ChannelType.public_thread:
+            await thread_responses.only_executable_in_threads(interaction, True)
+            return
+        await thread_responses.archived_thread(interaction, False)
+        await interaction.channel.edit(archived=True)
+
+    @app_commands.command(name="help", description="Mostra os comandos disponíveis")
+    async def help(self, interaction: discord.Interaction) -> None:
+        slash_commands = self.get_app_commands()
+        embed = discord.Embed(
+            title="Listinha de comandos",
+            description="Prefixo: *m!*",
+            color=discord.Color.blue()
+        )
+        embed.add_field(name="Comandos com prefixo", value="", inline=False)
+        for command in self.client.commands:
+            embed.add_field(name=f"m! {command.name}", value=command.description, inline=False)
+
+        embed.add_field(name="----------------", value="")
+        
+        embed.add_field(name="Comandos com barra", value="", inline=False)
+        for slash_command in slash_commands:
+            embed.add_field(name=f"/{slash_command.name}", value=slash_command.description, inline=False)
+
+        await interaction.response.send_message(embed=embed, ephemeral=True, delete_after=60)
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Moderation(client))
