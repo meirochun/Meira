dbf90adbcfd2f2cd07fe94ef09d6cc33c0dc8e4d
meirochun <meirochun@gmail.com>
2024-01-21 03:01:52 -0300

    Added more things in about command.

--- cogs/about.py
+++ cogs/about.py
@@ -1,8 +1,10 @@
 import os
-import discord
 from datetime import datetime
+
+import discord
 from discord import app_commands
 from discord.ext import commands
+from discord.ext.commands import Context
 
 class About(commands.Cog, name='sobre'):
     def __init__(self, client: commands.Bot) -> None:
@@ -13,19 +15,31 @@ class About(commands.Cog, name='sobre'):
         await interaction.response.defer()
         embed = discord.Embed(
             title="Meira (Bot)",
-            description="Meira é um bot (possívelmente genérico) de open source.",
+            description="Meira é um bot (possívelmente genérico) open source.",
             color=discord.Color.blue()
         )
-        embed.add_field(name="GitHub", value="https://github.com/meirochun/meira", inline = True)
-        embed.add_field(name="Versão", value=os.getenv("meira_version"), inline = True)
-
-        owner_avatar_url = await get_owner_avatar(self.client, os.getenv("owner_id"))
-        embed.set_footer(text=f"Desenvolvido por meirochou - {datetime.now().year}",
-            icon_url = owner_avatar_url)
+        embed.add_field(name="Prefixo", value="m!")
+        embed.add_field(name="Versão", value=os.getenv("MEIRA_VERSION"))
+        owner_avatar_url = await get_owner_avatar(self.client)
+        embed.set_footer(
+            text=f"Desenvolvido por meirochou | 2023-{datetime.now().year}",
+            icon_url = owner_avatar_url
+        )
 
         await interaction.followup.send(embed=embed)
 
-async def get_owner_avatar(client: commands.Bot, owner_id: str) -> str:
+    @commands.command(name="ping", description="Retorna o ping do bot")
+    async def ping(self, ctx: Context):
+        if ctx.author.bot:
+            return
+
+        await ctx.reply(
+            f"Pong! {round(ctx.bot.latency * 1000)}ms.",
+            delete_after=10,
+            ephemeral=True)
+
+async def get_owner_avatar(client: commands.Bot) -> str:
+    owner_id = os.getenv("OWNER_ID")
     owner = await client.fetch_user(int(owner_id))
     return owner.avatar.url
 
